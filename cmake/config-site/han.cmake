329b8faaf0ae7a5d36072e972645d31e141a9ecb
Mengjiao Han <mengjiao@han.sci.utah.edu>
2017-06-14 12:24:48 -0600

    update gitignore

---
+++ cmake/config-site/han.cmake
@@ -0,0 +1,10 @@
+#
+# configuration for hastur
+#
+set(embree_DIR /home/sci/mengjiao/Desktop/external/embree-2.15.0.x86_64.linux)
+set(ospray_DIR /home/sci/mengjiao/Desktop/ospray/install/lib64/cmake/ospray-1.3.0)
+set(TBB_ROOT /home/sci/mengjiao/Desktop/external/tbb2017_20170118oss)
+set(ISPC_EXECUTABLE /home/sci/mengjiao/Desktop/external/ispc-v1.9.1-linux)
+#
+# don't want it to appear in the cmake main window
+mark_as_advanced(embree_DIR ospray_DIR)
