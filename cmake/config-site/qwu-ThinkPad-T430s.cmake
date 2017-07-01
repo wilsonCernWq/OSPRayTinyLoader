f7fbaaa7bb39f9faf9ec5c32534a0f131b0630a7
Qi WU <wilson.over.cloud@gmail.com>
2017-06-30 18:43:07 -0600

    fixed compiling issues

---
+++ cmake/config-site/qwu-ThinkPad-T430s.cmake
@@ -0,0 +1,11 @@
+#
+# configuration for hastur
+#   written by Qi, WU 28 Feb 2017
+#
+set(ospray_DIR ~/work/ospray/install/lib/cmake/ospray-1.3.0)
+set(embree_DIR ~/software/embree-2.15.0.x86_64.linux)
+set(TBB_ROOT ~/software/tbb2017_20160916oss)
+set(ISPC_EXECUTABLE ~/software/ispc-v1.9.1-linux)
+#
+# don't want it to appear in the cmake main window
+# mark_as_advanced(embree_DIR ospray_DIR)
